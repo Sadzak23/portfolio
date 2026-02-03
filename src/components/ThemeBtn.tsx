9ed62287423154ae9bf9ffc698973e0cb855edbd
Sadzak23 <sadzak23@gmail.com>
2026-02-03 21:52:52 +0100

    Create theme toggle button

---
+++ src/components/ThemeBtn.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState, type FC } from "react";
+
+export const ThemeBtn: FC = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: 1 }}
+      className="toggle-btn"
+    >
+      <button
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        aria-label="Toggle theme"
+      >
+        {theme === "dark" ? <Sun /> : <Moon />}
+      </button>
+    </motion.div>
+  );
+};

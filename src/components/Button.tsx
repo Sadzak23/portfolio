1bf8b1705f71371a594a2996f9b8a20d94e58e69
Sadzak23 <sadzak23@gmail.com>
2026-02-06 01:56:31 +0100

    Add optional prop children to the Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,26 @@
-import type { FC } from "react";
+import type { FC, ReactNode } from "react";
 
 interface Props {
-  label: string;
+  label?: string;
   onClick?: () => void;
   disabled?: boolean;
   type?: HTMLButtonElement["type"];
+  children?: ReactNode;
 }
 
-export const Button: FC<Props> = ({ label, onClick, disabled, type }) => (
-  <button className="button" onClick={onClick} disabled={disabled} type={type}>
-    {label}
+export const Button: FC<Props> = ({
+  label,
+  onClick,
+  disabled,
+  type,
+  children,
+}) => (
+  <button
+    className="button flex center gap-2"
+    onClick={onClick}
+    disabled={disabled}
+    type={type}
+  >
+    {children ?? label}
   </button>
 );
